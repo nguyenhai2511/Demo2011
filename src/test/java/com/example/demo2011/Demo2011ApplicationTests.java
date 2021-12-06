7365d0174627fd4902294177e5acb214705e7607
gitName <a@b.c>
2021-12-06 11:17:18 +0700

    first commit

---
+++ src/test/java/com/example/demo2011/Demo2011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2011ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
