b1ba6bcb998a6dd084e6fb13869512d1af8315da
gitName <a@b.c>
2021-12-14 13:58:21 +0700

    demo

--- src/main/java/com/example/demo2011/Demo2011Application.java
+++ src/main/java/com/example/demo2011/Demo2011Application.java
@@ -2,10 +2,17 @@ package com.example.demo2011;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo2011Application {
 
+    @GetMapping("/")
+    public String index(){
+        return "WELCOME TO AZURE";
+    }
     public static void main(String[] args) {
         SpringApplication.run(Demo2011Application.class, args);
     }
